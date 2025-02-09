2f9d56857f6c8cee93f6f76b19ebc13730a382f4
RML-Cambo <ltomusic69@gmail.com>
2025-02-09 19:52:44 +0700

    Initialize

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+import { NavigationMenu } from "./components/ui/navigation-menu";
+import { NavigationMenuDemo } from "./components/demo/NavigationMenuDemo";
+
+function App() {
+  return (
+    <div className="p-5">
+      <Button>Hello Shadcn</Button>
+      <NavigationMenuDemo />
+    </div>
+  );
+}
+
+export default App;
