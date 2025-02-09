3986daa17ce78e6ef3bdc0cdb84cbcb399e4e3a2
RML-Cambo <ltomusic69@gmail.com>
2025-02-09 23:38:58 +0700

    new-feature-lib

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Button } from "./components/ui/button";
 import { NavigationMenu } from "./components/ui/navigation-menu";
 import { NavigationMenuDemo } from "./components/demo/NavigationMenuDemo";
+import { TabsDemo } from "./components/demo/TabsDemo";
 
 function App() {
   return (
     <div className="p-5">
       <Button>Hello Shadcn</Button>
       <NavigationMenuDemo />
+      <TabsDemo />
     </div>
   );
 }
